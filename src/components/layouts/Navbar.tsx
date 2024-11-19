b0e18b008923c4dfbdb2899f1c8a952b6f5aedb9
itisMilob <132344704+itismilob@users.noreply.github.com>
2024-11-19 19:20:47 +0900

    Revert "Revert 3 develop detail"

---
+++ src/components/layouts/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Image, TouchableOpacity, View } from 'react-native';
+import FontText from '../theme/FontText';
+
+export default function Navbar() {
+  return (
+    <View className="h-full w-full flex-row justify-between bg-gray-300 px-10">
+      <TouchableOpacity className="flex-1 items-center justify-center">
+        <Image />
+        <FontText>홈</FontText>
+      </TouchableOpacity>
+      <TouchableOpacity className="flex-1 items-center justify-center">
+        <Image />
+        <FontText>좋아요</FontText>
+      </TouchableOpacity>
+      <TouchableOpacity className="flex-1 items-center justify-center">
+        <Image />
+        <FontText>달력</FontText>
+      </TouchableOpacity>
+      <TouchableOpacity className="flex-1 items-center justify-center">
+        <Image />
+        <FontText>내 프로필</FontText>
+      </TouchableOpacity>
+    </View>
+  );
+}
