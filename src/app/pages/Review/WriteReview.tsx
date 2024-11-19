8c293f839a5eba279be0a2af6568ba982e3dd695
itismilob <itismilob@gmail.com>
2024-11-19 21:27:17 +0900

    feat: 이미지 입력받기

--- src/app/pages/Review/WriteReview.tsx
+++ src/app/pages/Review/WriteReview.tsx
@@ -2,18 +2,49 @@ import Header from '@/components/layouts/Header';
 import Profile from '@/components/Profile';
 import Stars from '@/components/Stars';
 import FontText from '@/components/theme/FontText';
+import { useState } from 'react';
 import { TextInput, TouchableOpacity, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { Image } from 'expo-image';
 
 export default function WriteReview() {
+  // 사용자 입력 이미지
+  const [image, setImage] = useState<string | null>(null);
+
+  // 사용자로부터 이미지를 입력받는 함수
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    // console.log(result);
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View className="flex-1">
       <Header left={<FontText>{'< 리뷰 작성하기'}</FontText>} />
       <View className="flex-1 m-7 gap-5">
         <Profile image="imgURL" nickname="닉네임" />
         <View className="flex-[5] bg-gray-200 items-center justify-center">
-          <TouchableOpacity className="h-10 w-10 bg-cyan-900 items-center justify-center rounded-full">
-            <FontText style={{ color: 'white' }}>+</FontText>
-          </TouchableOpacity>
+          {image ? (
+            <Image
+              source={{ uri: image }}
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            />
+          ) : (
+            <TouchableOpacity
+              onPress={pickImage}
+              className="h-10 w-10 bg-cyan-900 items-center justify-center rounded-full"
+            >
+              <FontText style={{ color: 'white' }}>+</FontText>
+            </TouchableOpacity>
+          )}
         </View>
         <View className="flex-1 gap-3 justify-center">
           <FontText>이 축제에 얼마나 만족하시나요?</FontText>
@@ -21,6 +52,7 @@ export default function WriteReview() {
         </View>
         <TextInput
           multiline
+          textAlignVertical="top"
           className="flex-[3] border-2 p-2"
           placeholder="다른 고객님에게 도움이 되도록 축제에 대한 솔직한 평가를 남겨주세요.(축제와 관계 없는 내용, 지나친 비난은 관리자에 의해 삭제될 수 있습니다.)"
         />
