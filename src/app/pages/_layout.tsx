32e16ce8eaf4ef21e44b8767e96cc3c91ce559cb
itismilob <itismilob@gmail.com>
2024-11-19 19:05:46 +0900

    fix: header 레이아웃에서 삭제

--- src/app/pages/_layout.tsx
+++ src/app/pages/_layout.tsx
@@ -2,16 +2,13 @@ import Header from '@/components/layouts/Header';
 import Navbar from '@/components/layouts/Navbar';
 import FontText from '@/components/theme/FontText';
 import { Slot } from 'expo-router';
-import { View } from 'react-native';
+import { Platform, View } from 'react-native';
 
 export default function PagesLayout() {
   return (
     <View className="flex-1">
-      {/* 헤더 들어가는 부분 */}
-      {/* <View className="h-20 mt-20">
-        <Header left={<FontText>{'< Header'}</FontText>} />
-      </View> */}
-      {/* 멍청하게 헤더를 수정할 수 없게 해놔서 지우기 */}
+      {/* 아이폰 마진 */}
+      {Platform.OS === 'ios' ? <View className="mt-20"></View> : null}
 
       {/* Slot : 하위 페이지 들어가는 부분 */}
       <View className="flex-1">
