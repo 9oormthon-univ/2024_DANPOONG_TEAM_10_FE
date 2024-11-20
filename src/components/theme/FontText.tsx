7ca4bbef65483f09f054024a00091a88a3ad1d48
godong <yunsun4830@gmail.com>
2024-11-21 00:00:14 +0900

    design:fonttext 수정

--- src/components/theme/FontText.tsx
+++ src/components/theme/FontText.tsx
@@ -1,15 +1,25 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text } from 'react-native';
 
 interface ThemedTextProps {
-  children: string| React.ReactNode;
+  children: string | React.ReactNode;
   style?: object;
   color?: string;
   //   bold?: boolean | undefined;
+  className?: string;
 }
 
 // 폰트가 적용된 Text 컴포넌트
-export default function FontText({ children, style,  color }: ThemedTextProps) {
-  return <Text style={[style,{ color }]}>{children}</Text>;
+export default function FontText({
+  children,
+  style,
+  color,
+  className,
+}: ThemedTextProps) {
+  return (
+    <Text className={className} style={[style, { color }]}>
+      {children}
+    </Text>
+  );
 }
 
 const styles = StyleSheet.create({
