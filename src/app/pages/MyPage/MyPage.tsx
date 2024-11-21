b9ab18b6b0b0ac9d173165b39aab4646c5adcece
godong <yunsun4830@gmail.com>
2024-11-21 13:58:50 +0900

    feat:토글버튼

--- src/app/pages/MyPage/MyPage.tsx
+++ src/app/pages/MyPage/MyPage.tsx
@@ -1,61 +1,71 @@
-
+import React, { useState } from 'react';
+import {TouchableOpacity, Alert, Image, SafeAreaView, ScrollView, View } from 'react-native';
 import FontText from '@/components/theme/FontText';
-import { Alert, Image, SafeAreaView, ScrollView, View, TouchableOpacity } from 'react-native';
 import Hr from '@/components/Hr';
-import { router } from 'expo-router';
 import Entypo from '@expo/vector-icons/Entypo';
+import ToggleButton from './ToggleButton'; // Import the ToggleButton component
 
 export default function MyPage() {
+    const [isOn, setIsOn] = useState(false);
+
+    const handleToggle = (value: boolean) => {
+      setIsOn(value);
+    };
+  
 
   return (
     <SafeAreaView>
-     <ScrollView> 
-            <View className="w-full p-3 justify-center mt-4 mb-3 ml-5" >
-                <View className='flex-row items-center gap-4'>
-                    <Image source={require('@/assets/danpong.png')}/>
-                    <View className='flex-col gap-2'>
-                        <FontText color="#000" style={{fontSize:20, fontWeight:800}}>닉네임</FontText>
-                        <FontText color="#000" style={{fontSize:12}}>카카오로그인</FontText>
-                            <TouchableOpacity
-                            className="w-30 bg-gray-100 border-2 border-gray-200 items-center rounded-xl px-5 py-1"
-                            onPress={() => Alert.alert("상세페이지")}>
-                                <FontText color="#1B1D1F" style={{fontSize:16, fontWeight:700}}>프로필 관리</FontText>
-                            </TouchableOpacity>
-                        
-                        
-                    </View>
-                </View>
+      <ScrollView>
+        <View className="w-full p-3 justify-center mt-4 mb-3 ml-5">
+          <View className="flex-row items-center gap-4">
+            <Image source={require('@/assets/danpong.png')} />
+            <View className="flex-col gap-2">
+              <FontText color="#000" style={{ fontSize: 20, fontWeight: '800' }}>
+                닉네임
+              </FontText>
+              <FontText color="#000" style={{ fontSize: 12 }}>
+                카카오로그인
+              </FontText>
+              <TouchableOpacity
+                className="w-30 bg-gray-100 border-2 border-gray-200 items-center rounded-xl px-5 py-1"
+                onPress={() => Alert.alert('상세페이지')}
+              >
+                <FontText color="#1B1D1F" style={{ fontSize: 16, fontWeight: '700' }}>
+                  프로필 관리
+                </FontText>
+              </TouchableOpacity>
             </View>
-            <Hr />
-            {/* 내용 */}
-           <View className='w-full px-5 gap-6 mt-10'>
-            <View className='flex-row justify-between'>
-             <FontText style={{fontSize:20, fontWeight:800}}>알람 설정</FontText>
-             <Entypo name="chevron-right" size={16} color="#1B1D1F" />    
-            </View> 
-            <View className='flex-row justify-between'>
-             <FontText style={{fontSize:20, fontWeight:800}}>개인 정보 관리</FontText>
-             <Entypo name="chevron-right" size={16} color="#1B1D1F" />    
-            </View> 
-            <View className='flex-row justify-between'>
-             <FontText style={{fontSize:20, fontWeight:800}}>고객 센터</FontText>
-             <Entypo name="chevron-right" size={16} color="#1B1D1F" />    
-            </View> 
-            <View className='flex-row justify-between'>
-             <FontText style={{fontSize:20, fontWeight:800}}>제휴 문의</FontText>
-             <Entypo name="chevron-right" size={16} color="#1B1D1F" />    
-            </View> 
-            <View className='flex-row justify-between'>
-             <FontText style={{fontSize:20, fontWeight:800}}>약관 및 정책</FontText>
-             <Entypo name="chevron-right" size={16} color="#1B1D1F" />    
-            </View> 
-            <View className='flex-row justify-between'>
-             <FontText style={{fontSize:20, fontWeight:800}}>공지 사항</FontText>
-             <Entypo name="chevron-right" size={16} color="#1B1D1F" />    
-            </View>      
-           </View>
+          </View>
+        </View>
+        <Hr />
+        <View className="w-full px-5 gap-6 mt-10">
+          <View className="flex-row justify-between items-center">
+            <FontText style={{ fontSize: 20, fontWeight: '800' }}>알람 설정</FontText>
+            {/* Use ToggleButton component */}
+            <ToggleButton isOn={isOn} onToggle={handleToggle} />
+          </View>
+          <View className="flex-row justify-between">
+            <FontText style={{ fontSize: 20, fontWeight: '800' }}>개인 정보 관리</FontText>
+            <Entypo name="chevron-right" size={16} color="#1B1D1F" />
+          </View>
+          <View className="flex-row justify-between">
+            <FontText style={{ fontSize: 20, fontWeight: '800' }}>고객 센터</FontText>
+            <Entypo name="chevron-right" size={16} color="#1B1D1F" />
+          </View>
+          <View className="flex-row justify-between">
+            <FontText style={{ fontSize: 20, fontWeight: '800' }}>제휴 문의</FontText>
+            <Entypo name="chevron-right" size={16} color="#1B1D1F" />
+          </View>
+          <View className="flex-row justify-between">
+            <FontText style={{ fontSize: 20, fontWeight: '800' }}>약관 및 정책</FontText>
+            <Entypo name="chevron-right" size={16} color="#1B1D1F" />
+          </View>
+          <View className="flex-row justify-between">
+            <FontText style={{ fontSize: 20, fontWeight: '800' }}>공지 사항</FontText>
+            <Entypo name="chevron-right" size={16} color="#1B1D1F" />
+          </View>
+        </View>
       </ScrollView>
     </SafeAreaView>
-    
   );
 }
---
+++ src/app/pages/MyPage/ToggleButton.tsx
@@ -0,0 +1,99 @@
+import React, { useRef, useEffect } from 'react';
+import { Text, View, Animated, TouchableOpacity, Easing } from 'react-native';
+
+interface ToggleButtonProps {
+  isOn: boolean;
+  onToggle: (value: boolean) => void;
+}
+
+export default function ToggleButton ({ isOn, onToggle }:ToggleButtonProps) {
+  const positionButton = useRef(new Animated.Value(isOn ? 1 : 0)).current;
+
+  useEffect(() => {
+    Animated.timing(positionButton, {
+      toValue: isOn ? 1 : 0,
+      duration: 300,
+      easing: Easing.ease,
+      useNativeDriver: false,
+    }).start();
+  }, [isOn, positionButton]);
+
+  const positionInterPol = positionButton.interpolate({
+    inputRange: [0, 1],
+    outputRange: [3, 27], // Position for the button's circle
+  });
+
+  const backgroundColorAnim = positionButton.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['#E9EBED', '#053C57'], // Background color of the toggle
+  });
+
+  const buttonColorAnim = positionButton.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['#053C57', '#FFF'], // Color of the button circle (moving part)
+  });
+
+  const initialOpacityOn = positionButton.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 1],
+  });
+
+  const initialOpacityOff = positionButton.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0],
+  });
+
+  const onPress = () => {
+    onToggle(!isOn);
+  };
+
+  return (
+    <TouchableOpacity
+      style={{ height: 30, width: 60 }}
+      activeOpacity={0.9}
+      onPress={onPress}
+    >
+      <Animated.View
+        style={[
+          { height: 30, width: 60, borderRadius: 30 },
+          { backgroundColor: backgroundColorAnim },
+        ]}
+        className="relative"
+      >
+        <Animated.Text
+          style={[
+            { position: 'absolute', top: 6, left: 5, fontSize: 14 },
+            { color: '#fff' },
+            { opacity: initialOpacityOn },
+          ]}
+        >
+          ON
+        </Animated.Text>
+        <Animated.Text
+          style={[
+            { position: 'absolute', top: 6, right: 5, fontSize: 14 },
+            { color: '#053C57' },
+            { opacity: initialOpacityOff },
+          ]}
+        >
+          OFF
+        </Animated.Text>
+        <Animated.View
+          style={[
+            {
+              height: 15,
+              width: 15,
+              borderRadius: 20,
+              margin:8,
+              
+            },
+            {
+              backgroundColor: buttonColorAnim,
+              transform: [{ translateX: positionInterPol }],
+            },
+          ]}
+        />
+      </Animated.View>
+    </TouchableOpacity>
+  );
+};
