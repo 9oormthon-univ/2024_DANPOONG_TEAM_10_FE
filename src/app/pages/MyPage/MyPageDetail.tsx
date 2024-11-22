bcb488dba6098ebc3cf7c549a6cf82866c1719b9
godong <yunsun4830@gmail.com>
2024-11-23 03:49:35 +0900

    feat: 알림설정 AsyncStorate 저장, 상세 마이페이지 UI 수정

--- src/app/pages/MyPage/MyPageDetail.tsx
+++ src/app/pages/MyPage/MyPageDetail.tsx
@@ -9,41 +9,47 @@ import { router } from "expo-router";
 export default function MyPageDetail() {
   const [name, setName] = useState<string>("");
   const [isValid, setIsValid] = useState<boolean>(false);
-  const [profileImage, setProfileImage] = useState<string | null>(null); // Profile image state
+  const [profileImage, setProfileImage] = useState<string | null>(null);
 
   useEffect(() => {
     const loadProfileImage = async () => {
       const storedImage = await AsyncStorage.getItem('profileImage');
       if (storedImage) {
-        setProfileImage(storedImage); // Load profile image from AsyncStorage
+        setProfileImage(storedImage);
       }
 
-      const storedName = await AsyncStorage.getItem('userName'); // Fetch userName from AsyncStorage
+      const storedName = await AsyncStorage.getItem('userName');
       if (storedName) {
-        setName(storedName); // If userName exists, set it
+        setName(storedName); 
       }
     };
 
-    loadProfileImage(); // Load profile image and userName
+    loadProfileImage(); 
   }, []);
 
   return (
     <SafeAreaView className="flex-1 w-full items-center">
       {/* Profile Component */}
+      <View className="mt-[60px]">
       <Profile initialProfileImage={profileImage} />
-
+      </View>
       <View className="mt-11 mb-10 px-6">
         <SignupForm
           name={name}
           onNameChange={(newName) => setName(newName)}
           onValidChange={(valid) => setIsValid(valid)}
-          placeholder={name || "닉네임을 입력하세요"} // Set dynamic placeholder
+          placeholder={name || "닉네임을 입력하세요"}
         />
       </View>
 
+      <View className="w-full px-5 mt-[77px]">
+        <FontText className="font-base text-[12px] text-[#9FA4A9]">
+        프로필 정보(이름, 사진)는 리뷰 작성 시 표시됩니다.
+        </FontText>
+      </View>
       {/* Submit Button */}
       <TouchableOpacity
-        className={`w-2/5 justify-center rounded-full py-2 items-center mt-40 mb-3 ${
+        className={`w-2/5 justify-center rounded-full py-2 items-center mt-[24px] mb-3 ${
           isValid
             ? "bg-[#053C57]"
             : "border-[1px] border-[#053C57] bg-[#F7F8F9]"
