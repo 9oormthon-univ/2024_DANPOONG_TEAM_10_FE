24467df0081180a8d8b4f19693343371d4efec4e
itismilob <itismilob@gmail.com>
2024-11-23 00:06:55 +0900

    feat: 데이터 연결 준비

--- src/app/pages/CalendarPage.tsx
+++ src/app/pages/CalendarPage.tsx
@@ -2,12 +2,18 @@ import Header from '@/components/layouts/Header';
 import FontText from '@/components/theme/FontText';
 import { DropDownItems } from '@/utils/Types';
 import { useEffect, useReducer, useState } from 'react';
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { Calendar, LocaleConfig } from 'react-native-calendars';
 import DropDownPicker from 'react-native-dropdown-picker';
 
 type MarkedDateType = {
   [key: string]: Object;
+};
+
+type CalendarData = {
+  date: Date;
+  type: string;
+  id: number;
 };
 
 export default function CalendarPage() {
@@ -21,10 +27,13 @@ export default function CalendarPage() {
   // 드롭다운에 들어가는 옵션 (라벨, 값)
   const [yearItems, setYearItems] = useState<DropDownItems[]>([]);
   const [monthItems, setMonthItems] = useState<DropDownItems[]>([]);
+  const [date, setDate] = useState<number>(0);
 
   // 달력의 현재 날짜
-  const [currentDate, setCurrentDate] = useState<string>('');
-  // 달력에 표시되는 마커
+  const [currentDate, setCurrentDate] = useState<string>('2000-01-01');
+  // 달력 데이터 (월별 데이터)
+  const [calendarDataList, setCalendarDataList] = useState<CalendarData[]>([]);
+  // 달력에 표시되는 마커 정보
   const [markedDate, setMarkedDate] = useState<MarkedDateType>();
 
   // 년 드롭다운 열기
@@ -109,7 +118,13 @@ export default function CalendarPage() {
     },
   };
 
+  const dateClickHandler = () => {};
+  const eventClickHandler = () => {};
+
+  // 데이터 불러오기
   const fetchData = async () => {
+    setCalendarDataList;
+    // calendarData의 type에 맞춰서 좋아요/축제 스타일 설정하기
     setMarkedDate({
       '2024-11-19': markedDateStyle,
       '2024-11-23': selectedDateStyle,
@@ -118,20 +133,23 @@ export default function CalendarPage() {
     });
   };
 
+  // 년/월이 변경되면 날짜 변경
   useEffect(() => {
     const formatedMonth = month.length === 1 ? '0' + month : month;
     setCurrentDate(`${year}-${formatedMonth}-01`);
   }, [year, month]);
 
+  // 첫 랜더링
   useEffect(() => {
     // 드롭다운 범위 지정
     const today = new Date();
+    const thisYear = today.getFullYear();
     const thisMonth = today.getMonth() + 1;
-    setYear(today.getFullYear().toString());
+    setYear(thisYear.toString());
     setMonth(thisMonth.toString());
-    // 2000년 이후 ~
+    // 2000년 이후 ~ 현재 년도 + 10년
     const yearArray = Array.from({
-      length: today.getFullYear() - 1999,
+      length: thisYear + 10 - 1999,
     })
       .map((_, i) => {
         return {
@@ -154,11 +172,12 @@ export default function CalendarPage() {
     setMonthItems(monthArray);
 
     fetchData();
-    setCurrentDate(new Date().toString());
+    // 현재 날짜로 초기화 (렌더링 문제때문에 해야함)
+    setCurrentDate(new Date('2000-01-01').toString());
   }, []);
 
   return (
-    <View>
+    <View className="flex-1">
       {/* 드롭다운 년/월 선택 */}
       <Header
         left={
@@ -209,22 +228,36 @@ export default function CalendarPage() {
         }
       />
       {/* 달력 */}
-      <Calendar
-        current={currentDate}
-        key={currentDate}
-        renderHeader={() => null}
-        hideArrows={true}
-        markingType={'custom'}
-        markedDates={markedDate}
-        theme={{
-          textDayHeaderFontSize: 20,
-          textDayHeaderFontWeight: 'bold',
-          // todayTextColor: '#053C57',
-          todayTextColor: '#ffffff',
-          todayBackgroundColor: '#c82a2a',
-        }}
-      />
-      <View></View>
+      <View className="px-5">
+        <Calendar
+          current={currentDate}
+          key={currentDate}
+          renderHeader={() => null}
+          hideArrows={true}
+          markingType={'custom'}
+          markedDates={markedDate}
+          theme={{
+            textDayHeaderFontSize: 20,
+            textDayHeaderFontWeight: 'bold',
+            // todayTextColor: '#053C57',
+            todayTextColor: '#ffffff',
+            todayBackgroundColor: '#c82a2a',
+          }}
+          onDayPress={(day) => {
+            setDate(day.day);
+          }}
+        />
+      </View>
+      <View className="flex-1 h-full p-5">
+        <View className="flex-1 p-5 bg-gray-300 rounded-xl">
+          <FontText className="font-bold text-2xl">
+            {currentDate.split('-').join('. ')}
+          </FontText>
+          <ScrollView>
+            {/* 월별 데이터의 date에 맞게 데이터 리스트 */}
+          </ScrollView>
+        </View>
+      </View>
     </View>
   );
 }
